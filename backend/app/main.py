8cde860ce5696fac7785a3f5915a75974e556ee4
mihir120104 <mihirpatel1201@gmail.com>
2026-07-23 20:54:23 +0530

    fix: update CORS for production

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
-
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routes.health import router as health_router
+
 from app.database.base import Base
 from app.database.session import engine
+
+from app.api.routes.health import router as health_router
 from app.api.routes.auth import router as auth_router
-# Import all models so SQLAlchemy knows about them
-from app.models.user import User
-from app.api.routes.dashboard import router as dashboard_router
 from app.api.routes.vehicle import router as vehicle_router
 from app.api.routes.purchase import router as purchase_router
+from app.api.routes.dashboard import router as dashboard_router
+
+# Import models so SQLAlchemy creates all tables
+from app.models.user import User
+from app.models.vehicle import Vehicle
+from app.models.purchase import Purchase
+
 app = FastAPI(
     title="AutoVerse API",
     version="1.0.0",
@@ -18,9 +23,15 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        # Local Development
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://auto-verse-7r7ya752c-mihir-patels-projects-aefe17c5.vercel.app",
+
+        # Vercel Production Domain
+        "https://auto-verse-sigma.vercel.app",
+
+        # Current Vercel Deployment
+        "https://auto-verse-f139q69mj-mihir-patels-projects-aefe17c5.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -35,9 +46,10 @@ app.include_router(vehicle_router)
 app.include_router(purchase_router)
 app.include_router(dashboard_router)
 
+
 @app.get("/")
 def root():
     return {
-        "message": "Welcome to AutoVerse API ",
+        "message": "Welcome to AutoVerse API",
         "version": "1.0.0",
     }
